50abb5654be9404bb1b276aa66a8e3b09ab1d7d9
Ivan Peng <ipeng@usc.edu>
2020-04-10 17:25:48 -0700

    Add files via upload

---
+++ Coditor/src/util/AccessLevelOutOfBoundsException.java
@@ -0,0 +1,10 @@
+package util;
+
+public class AccessLevelOutOfBoundsException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AccessLevelOutOfBoundsException(String message) {
+        super(message);
+    }
+}
