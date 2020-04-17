38affb3bc57c136bfd94481f0f361cefe625bf2b
Ivan Peng <ipeng@usc.edu>
2020-04-17 00:32:35 -0700

    Added verifyUser, removeDocument, and docExists

--- Database.java
+++ Database.java
@@ -22,13 +22,18 @@ public class Database {
 			System.out.println("Exception in connecting to database: " + sqle.getMessage());
 		}
 		
-		//addUser(1, "insertTest", "badpw");
-		//addDocument(4, "testadd", 1);
-		//addDocument(3, "add2", 1);
-		//addDocument(5, "add3", 1);
-		//removeUser(1);
-		//addUser(32, "testEmail@gmail.com", "1231");	
-		shareDocument(11, 2);
+		// addUser(1, "insertTest", "badpw");
+		// addDocument(4, "testadd", 1);
+		// addDocument(3, "add2", 1);
+		// addDocument(5, "add3", 1);
+		// removeUser(1);
+		// addUser(32, "testEmail@gmail.com", "1231");	
+		// shareDocument(11, 2);
+		// docExists(4);
+		// docExists(6);
+		// verifyUser(4, 1);
+		// verifyUser(4, 0);
+		// removeDocument(4);
 	}
 	
 	public static void addUser(int userID, String userEmail, String userPW) {
@@ -98,7 +103,23 @@ public class Database {
 		}
 	}
 
-	public void removeDocument(int documentID) { //deletes a single document, removes it from all shared users' drives
+	public static void removeDocument(int docID) { //deletes a single document, removes it from all shared users' drives
+		try {
+			if (docExists(docID)) {
+				String r = "DELETE FROM Document WHERE docID =" + docID + ";";
+				ps = conn.prepareStatement(r);
+				ps.execute();
+				r = "DELETE FROM Master WHERE docID =" + docID + ";";
+				ps = conn.prepareStatement(r);
+				ps.execute();
+			}
+			else {
+				System.err.println("Can't remove Document " + docID + " because it doesn't exist.");
+			}
+			
+		} catch (SQLException sqle) {
+			System.out.println("Error in removing a user: " + sqle.getMessage());
+		}
 	}
 	
 	public static void shareDocument(int docID, int userID) {//adds specified user to list of valid users for a particular document
@@ -111,8 +132,43 @@ public class Database {
 		}
 	}
 	
-	public boolean verifyUse(int docID, int userID) { //checks whether or not this user has access to a document
-		return true;
+	public static boolean verifyUser(int docID, int userID) { //checks whether or not this user has access to a document
+		try {
+			rs = st.executeQuery("SELECT docID FROM Master WHERE userID =" + userID);
+			while (rs.next()) {
+				String temp = rs.getString("docID");
+				if (Integer.parseInt(temp) == docID) {
+					System.err.println("User " + userID + " has access to Document " + docID);
+					return true;
+				}
+			}
+		} catch (NumberFormatException e) {
+			System.err.println ("NumberFormatException in docExists(): " + e.getMessage());
+		} catch (SQLException e) {
+			System.err.println ("SQLException in docExists(): " + e.getMessage());
+		}
+		System.err.println("User " + userID + " does not have access to Document " + docID);
+		return false;
+	}
+
+	private static boolean docExists(int DocID) {
+		try {
+			rs = st.executeQuery("SELECT docID FROM Document");
+			while (rs.next()) {
+				String temp = rs.getString("docID");
+				int nextID = Integer.parseInt(temp);
+				if (nextID == DocID) { 
+					System.err.println("Document " + DocID + " found!");
+					return true;
+				}
+			}
+		} catch (NumberFormatException e) {
+			System.err.println ("NumberFormatException in docExists(): " + e.getMessage());
+		} catch (SQLException e) {
+			System.err.println ("SQLException in docExists(): " + e.getMessage());
+		}
+		System.err.println("Document " + DocID + " not found!");
+		return false;
 	}
 	
 	public void databaseUseComplete() { //only use when we are completely done and ready to close database connection
