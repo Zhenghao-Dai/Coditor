86760317a9f1586234590640c71a4769a127cfc6
Zhenghao (Lucas) Dai <zhenghad@usc.edu>
2020-04-19 08:15:06 +0800

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,69 @@
+package coditor;
+
+
+import java.io.*;
+import java.sql.SQLException;
+ 
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+ 
+@WebServlet("/login")
+public class Server extends HttpServlet {
+ 
+    public Server() {
+        super();
+    }
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+         
+        UserDAO userDao = new UserDAO();
+         
+        try {
+            User user = userDao.checkLogin(email, password);
+            String destPage = "login.jsp";
+             
+            if (user != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", user);
+                destPage = "home.jsp";
+            } else {
+                String message = "Invalid email/password";
+                request.setAttribute("message", message);
+            }
+            
+            
+            //destPage.redirect to 
+            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+            dispatcher.forward(request, response);
+             
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new ServletException(ex);
+        }
+    }
+}
+
+@WebServlet("/logout")
+public class UserLogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+ 
+    public UserLogoutServlet() {
+        super();
+    }
+ 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute("user");
+             
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
+
+
