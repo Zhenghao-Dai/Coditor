4204f393503b69870a2a0cc599f6a832267cc127
Zhenghao (Lucas) Dai <zhenghad@usc.edu>
2020-04-20 13:32:31 +0800

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,49 @@
+package coditor;
+
+
+import java.io.*;
+import java.sql.SQLException;
+ 
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+ 
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+        super();
+    }
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        
+        Account user = new Account();
+         
+        //if not exist then return null
+		user = user.login(email, password);
+		String destPage = "login.jsp";
+		 
+		if (user != null) {
+		    HttpSession session = request.getSession();
+		    session.setAttribute("user", user);
+		    destPage = "home.jsp";
+		} else {
+		    String message = "Invalid email/password";
+		    request.setAttribute("message", message);
+		}
+		
+		
+		//destPage.redirect to 
+		RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+		dispatcher.forward(request, response);
+    }
+}
+
+
+
+
