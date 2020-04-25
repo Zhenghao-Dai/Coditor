4365e8a4931bd5debf7021986356567895e571bd
Zhenghao (Lucas) Dai <zhenghad@usc.edu>
2020-04-25 12:14:57 +0800

    Add files via upload

---
+++ GuestShare.java
@@ -0,0 +1,36 @@
+package coditor;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
+@WebServlet("/guestshare")
+public class GuestShare extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public GuestShare() {
+        super();
+    }
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("docId");
+        String password = request.getParameter("password");
+        
+		String destPage = "doc.jsp";
+		//Set div = innerhtml
+		
+		//destPage.redirect to 
+		RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+		dispatcher.forward(request, response);
+    }
+}
